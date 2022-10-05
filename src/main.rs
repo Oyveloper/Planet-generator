5791b00f4c857eb84dd1e77267bc849e0c3c23e2
Øyvind Monsen <yvind.monsen@gmail.com>
2022-10-05 23:19:00 +0900

    completed an interactive planet generation with 3 noise levels

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::f32::consts::FRAC_PI_2;
+use std::f32::consts::{FRAC_PI_2, TAU};
 
-use bevy::reflect::{FromReflect, TypeUuid};
+use bevy::reflect::TypeUuid;
 use bevy::render::mesh::Indices;
 use bevy::render::render_resource::PrimitiveTopology;
 use bevy::{
@@ -10,12 +10,13 @@ use bevy::{
     render::{mesh::VertexAttributeValues, render_resource::AsBindGroup},
 };
 
-use bevy_inspector_egui::{Inspectable, WorldInspectorPlugin};
+use bevy_inspector_egui::{Inspectable, InspectorPlugin};
 use bevy_rapier3d::prelude::*;
 
 use noise::NoiseFn;
 use noise::Perlin;
 
+use crate::planet::{NoiseSettings, PlanetGeneratorData, PlanetShape};
 mod planet;
 
 const TIME_STEP: f32 = 1.0 / 60.0;
@@ -27,8 +28,10 @@ fn main() {
         // .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         // .add_plugin(RapierDebugRenderPlugin::default())
-        .add_plugin(WorldInspectorPlugin::default())
+        .add_plugin(InspectorPlugin::<PlanetGeneratorData>::new())
         .add_startup_system(setup)
+        .add_system(generate)
+        .add_system(rotate_planet_system)
         .add_system(bevy::window::close_on_esc)
         // .add_startup_system(grab_mouse)
         .add_system(camera_move_system)
@@ -45,47 +48,8 @@ struct Camera {
     yaw: f32,
 }
 
-#[derive(Inspectable)]
-struct PlanetData {
-    radius: f32,
-    resolution: u8,
-    noise_settings: NoiseSetting,
-}
-
-#[derive(Inspectable, Clone)]
-struct NoiseSetting {
-    filters: Vec<NoiseFilter>,
-}
-
-#[derive(Component, Default, Clone, Copy, Inspectable)]
-struct NoiseFilter {
-    pub seed: Vec3,
-    pub scale: f64,
-    pub strength: f64,
-}
-
-impl NoiseFilter {
-    pub fn new(seed: Vec3, scale: f64, strength: f64) -> Self {
-        Self {
-            seed,
-            scale,
-            strength,
-        }
-    }
-
-    pub fn get_noise(&self, x: f64, y: f64, z: f64) -> f64 {
-        let mut base_noise_fn = Perlin::new();
-        base_noise_fn.get([
-            (x + self.seed.x as f64) * self.scale,
-            (y + self.seed.y as f64) * self.scale,
-            (z + self.seed.z as f64) * self.scale,
-        ]) * self.strength
-    }
-}
-
-#[derive(Component, Default, Reflect, Copy, Clone)]
-#[reflect(Component)]
-struct Planet {}
+#[derive(Component)]
+struct Planet;
 
 #[derive(AsBindGroup, TypeUuid, Clone)]
 #[uuid = "1319ad1c-420a-4c34-8abe-62d6508588c4"]
@@ -93,47 +57,53 @@ pub struct CoolMaterial {}
 
 impl Material for CoolMaterial {}
 
+fn generate(
+    data: Res<PlanetGeneratorData>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    query: Query<(&Planet, &Handle<Mesh>, &Handle<StandardMaterial>)>,
+) {
+    if !data.is_changed() {
+        return;
+    }
+
+    for (_, mesh, material) in query.iter() {
+        let mesh = meshes.get_mut(mesh).unwrap();
+        // let material = materials.get_mut(material).unwrap();
+
+        *mesh = PlanetShape::new(&data).into()
+    }
+}
+
+fn rotate_planet_system(
+    time: Res<Time>,
+    data: Res<PlanetGeneratorData>,
+    mut query: Query<&mut Transform>,
+) {
+    for mut transform in &mut query {
+        // transform.rotate_y(
+        //     // data.origin
+        //     // Quat::from_rotation_y(time.delta_seconds() * TAU * 0.2),
+        //     // time.delta_seconds() * TAU * 0.2,
+        // );
+    }
+}
+
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    planet_data: Res<PlanetGeneratorData>,
     // asset_server: Res<AssetServer>,
 ) {
-    let planet = Planet {
-        radius: 1.0,
-        seed: Vec3::new(0.0, 0.0, 0.0),
-        resolution: 200,
-        origin: Vec3::new(0.0, 0.0, 0.0),
-    };
-    let planes = generate_planet(&planet);
-    for plane in planes {
-        commands
-            .spawn_bundle(PbrBundle {
-                mesh: meshes.add(plane),
-                material: materials.add(Color::WHITE.into()),
-                ..default()
-            })
-            .insert(planet);
-    }
-    // Bouncing ball
     commands
         .spawn_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Icosphere {
-                radius: 1.0,
-                subdivisions: 6,
-            })),
-            material: materials.add(StandardMaterial {
-                base_color: Color::rgb(0.3, 0.6, 0.9).into(),
-                metallic: 1.0,
-                perceptual_roughness: 0.5,
-                ..Default::default()
-            }),
+            mesh: meshes.add(PlanetShape::new(&planet_data).into()),
+            material: materials.add(Color::WHITE.into()),
+            transform: Transform::from_translation(planet_data.origin),
             ..default()
         })
-        // .insert(RigidBody::Dynamic)
-        .insert(Collider::ball(1.))
-        .insert(Restitution::coefficient(0.7))
-        .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 14.0, 0.0)));
+        .insert(Planet);
 
     // Lighting
     const HALF_SIZE: f32 = 10.0;
@@ -167,7 +137,7 @@ fn setup(
     // camera
     commands
         .spawn_bundle(Camera3dBundle {
-            transform: Transform::from_xyz(0.0, 20.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(0.0, 20.0, 10.0).looking_at(planet_data.origin, Vec3::Y),
             ..default()
         })
         .insert(Camera {
@@ -175,138 +145,6 @@ fn setup(
             rotation_speed: 1.7,
             ..default()
         });
-}
-fn planet_generate_system(
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    query: Query<(&Planet, &Handle<Mesh>)>,
-) {
-    for (planet, mesh) in query.iter() {
-        let mesh = meshes.get_mut(mesh).unwrap();
-
-        *mesh = generate_planet(&planet)[0];
-        // material.base_color = data.color;
-    }
-}
-
-fn generate_planet(planet: &Planet) -> Vec<Mesh> {
-    let rotations = [
-        Mat3::from_rotation_x(0.0),
-        Mat3::from_rotation_x(std::f32::consts::PI / 2.0),
-        Mat3::from_rotation_x(-std::f32::consts::PI / 2.0),
-        Mat3::from_rotation_x(std::f32::consts::PI),
-        Mat3::from_rotation_y(std::f32::consts::PI / 2.0),
-        Mat3::from_rotation_y(-std::f32::consts::PI / 2.0),
-    ];
-
-    let noise = Perlin::new();
-
-    println!("{:?}", noise.get([0.0, 0.0, 0.0]));
-
-    rotations
-        .iter()
-        .map(|rot| {
-            generate_planet_side_plane(planet.resolution, planet.origin, planet.radius, *rot)
-        })
-        .collect()
-}
-
-fn calculate_normals_for_mesh(mesh: &Mesh, resolution: usize) -> Vec<[f32; 3]> {
-    let mut normals: Vec<[f32; 3]> = vec![];
-    if let Some(VertexAttributeValues::Float32x3(vertices)) =
-        mesh.attribute(Mesh::ATTRIBUTE_POSITION)
-    {
-        for (i, vertex) in vertices.iter().enumerate() {
-            let is_right_edge = i % resolution == resolution - 1;
-            let is_bottom_edge = i >= (resolution * (resolution - 1));
-
-            let vertex_vec = Vec3::new(vertex[0], vertex[1], vertex[2]);
-
-            let x_dir: i32 = if is_right_edge { -1 } else { 1 };
-            let y_dir: i32 = if is_bottom_edge { -1 } else { 1 };
-
-            let x_vertex = vertices[(i as i32 + x_dir) as usize];
-            let y_vertex = vertices[(i as i32 + y_dir * resolution as i32) as usize];
-
-            let x_dir_vec = Vec3::new(x_vertex[0], x_vertex[1], x_vertex[2]) - vertex_vec;
-            let y_dir_vec = Vec3::new(y_vertex[0], y_vertex[1], y_vertex[2]) - vertex_vec;
-
-            let normal = x_dir_vec.cross(y_dir_vec).normalize();
-
-            let normal_multiplier = normal.dot(Vec3::from(*vertex)).signum();
-
-            normals.push((normal * normal_multiplier).into());
-        }
-    }
-
-    normals
-}
-
-fn generate_planet_side_plane(
-    resolution: u8,
-    origin: Vec3,
-    planet_radius: f32,
-    rotation: Mat3,
-) -> Mesh {
-    let noise = Perlin::new();
-
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    let start = -1.;
-    let mut vertices: Vec<[f32; 3]> = vec![];
-    // let mut normals: Vec<[f32; 3]> = vec![];
-    let mut uvs: Vec<[f32; 2]> = vec![];
-    let mut colors: Vec<[f32; 4]> = vec![];
-    let mut index_values: Vec<u32> = vec![];
-    let mut current_index: u32 = 0;
-    for i in 0..resolution {
-        let y_percentage = i as f32 / (resolution - 1) as f32;
-        let is_bottom_edge = y_percentage == 1.;
-        for j in 0..resolution {
-            let x_percentage = j as f32 / (resolution - 1) as f32;
-            let is_right_edge = x_percentage == 1.;
-
-            let uv = [x_percentage, y_percentage];
-
-            let vertex_direction_vec = rotation
-                * Vec3::new(start + 2. * x_percentage, start + 2. * y_percentage, 1.).normalize();
-            let offset = noise.get([
-                vertex_direction_vec.x.into(),
-                vertex_direction_vec.y.into(),
-                vertex_direction_vec.z.into(),
-            ]) * 0.7;
-
-            let vertex_position = origin + vertex_direction_vec * (planet_radius + offset as f32);
-
-            vertices.extend([[vertex_position.x, -vertex_position.y, vertex_position.z]]);
-            uvs.push(uv);
-            colors.push([1.0, 0.8, 1.0, 1.0]);
-
-            if !(is_right_edge || is_bottom_edge) {
-                index_values.extend([
-                    current_index,
-                    current_index + resolution as u32,
-                    current_index + 1,
-                    current_index + resolution as u32,
-                    current_index + resolution as u32 + 1,
-                    current_index + 1,
-                ]);
-            }
-
-            current_index += 1;
-        }
-    }
-
-    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_NORMAL,
-        calculate_normals_for_mesh(&mesh, resolution as usize),
-    );
-    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
-    mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, colors);
-    mesh.set_indices(Some(Indices::U32(index_values)));
-    mesh.generate_tangents().unwrap();
-
-    mesh
 }
 
 fn camera_move_system(
--- src/planet.rs
+++ src/planet.rs
@@ -1,20 +1,216 @@
-use bevy::prelude::*;
+#![allow(clippy::type_complexity, clippy::identity_op)]
+use bevy::render::mesh::VertexAttributeValues;
+use bevy::render::render_resource::PrimitiveTopology;
+use bevy::{prelude::*, render::mesh::Indices};
 use bevy_inspector_egui::Inspectable;
+use noise::{NoiseFn, Perlin};
 
-#[derive(Debug, Clone, Default, Inspectable, Component)]
-struct Planet {
+#[derive(Debug, Clone, Inspectable)]
+pub struct PlanetGeneratorData {
+    pub radius: f64,
+    pub noise_settings: [NoiseSettings; 3],
+    pub resolution: i32,
+    pub origin: Vec3,
+}
+
+#[derive(Debug, Clone, Default, Inspectable)]
+pub struct NoiseSettings {
+    pub seed: u64,
+    pub origin: Vec3,
+    #[inspectable(min = 0.0)]
+    pub amplitude: f64,
+    #[inspectable(min = 0.0)]
+    pub frequency: f32,
+    pub mask_by_previous: bool,
+}
+
+impl Default for PlanetGeneratorData {
+    fn default() -> Self {
+        Self {
+            origin: Vec3::new(0.0, 10.0, -50.0),
+            radius: 20.0,
+            noise_settings: [
+                NoiseSettings {
+                    seed: 0,
+                    origin: Vec3::new(0.0, 0.0, 0.0),
+                    amplitude: 1.0,
+                    frequency: 1.0,
+                    mask_by_previous: false,
+                },
+                NoiseSettings {
+                    seed: 1,
+                    origin: Vec3::new(0.0, 0.0, 0.0),
+                    amplitude: 1.0,
+                    frequency: 1.0,
+                    mask_by_previous: false,
+                },
+                NoiseSettings {
+                    seed: 2,
+                    origin: Vec3::new(0.0, 0.0, 0.0),
+                    amplitude: 1.0,
+                    frequency: 1.0,
+                    mask_by_previous: false,
+                },
+            ],
+            resolution: 32,
+        }
+    }
+}
+
+pub struct PlanetShape {
+    resolution: u8,
     radius: f64,
-    origin: Vec3,
-    rasolution: u8,
-    noise_settings: NoiseSettings,
+    noise_settings: [NoiseSettings; 3],
 }
 
-#[derive(Debug, Clone, Default, Inspectable, Component)]
-struct NoiseSettings {
-    frequency: f64,
-    lacunarity: f64,
-    persistence: f64,
-    octaves: u8,
+impl PlanetShape {
+    pub fn new(data: &PlanetGeneratorData) -> Self {
+        Self {
+            resolution: data.resolution as u8,
+            radius: data.radius,
+            noise_settings: data.noise_settings.clone(),
+        }
+    }
+
+    fn get_noise_value(&self, noise_fn: Perlin, vertex_direction_vec: Vec3) -> f64 {
+        let mut noise_value = 0.0;
+        for noise_setting in self.noise_settings.iter() {
+            if noise_setting.mask_by_previous && noise_value == 0.0 {
+                continue;
+            }
+            let noise_orig = noise_setting.origin + vertex_direction_vec * noise_setting.frequency;
+            let n = noise_fn.get([
+                noise_orig.x.into(),
+                noise_orig.y.into(),
+                noise_orig.z.into(),
+            ]) * noise_setting.amplitude;
+
+            noise_value += n;
+        }
+
+        noise_value.max(0.0)
+    }
+
+    fn generate_mesh(&self) -> Mesh {
+        // Noise funciton to use for all sampling
+        let noise = Perlin::new();
+        let resolution = self.resolution;
+
+        // Mesh data initialization
+        let mut uvs: Vec<[f32; 2]> = vec![];
+        let mut colors: Vec<[f32; 4]> = vec![];
+        let mut index_values: Vec<u32> = vec![];
+        let mut normals: Vec<[f32; 3]> = vec![];
+        let mut vertices: Vec<[f32; 3]> = vec![];
+
+        let origin = Vec3::new(0.0, 0.0, 0.0);
+
+        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+        let start = -1.;
+        // let mut normals: Vec<[f32; 3]> = vec![];
+        let mut current_index: u32 = 0;
+
+        let rotations = [
+            Mat3::from_rotation_x(0.0),
+            Mat3::from_rotation_x(std::f32::consts::PI / 2.0),
+            Mat3::from_rotation_x(-std::f32::consts::PI / 2.0),
+            Mat3::from_rotation_x(std::f32::consts::PI),
+            Mat3::from_rotation_y(std::f32::consts::PI / 2.0),
+            Mat3::from_rotation_y(-std::f32::consts::PI / 2.0),
+        ];
+
+        for &rotation in &rotations {
+            for y in 0..resolution {
+                let y_percentage = y as f32 / (resolution - 1) as f32;
+                let is_bottom_edge = y_percentage == 1.;
+
+                for x in 0..resolution {
+                    let x_percentage = x as f32 / (resolution - 1) as f32;
+                    let is_right_edge = x_percentage == 1.;
+
+                    let uv = [x_percentage, y_percentage];
+
+                    let vertex_direction_vec = rotation
+                        * Vec3::new(start + 2. * x_percentage, start + 2. * y_percentage, 1.)
+                            .normalize();
+                    let offset = self.get_noise_value(noise, vertex_direction_vec);
+
+                    let vertex_position =
+                        origin + vertex_direction_vec * ((self.radius + offset) as f32);
+
+                    vertices.extend([[vertex_position.x, -vertex_position.y, vertex_position.z]]);
+                    uvs.push(uv);
+                    colors.push([1.0, 0.8, 1.0, 1.0]);
+
+                    normals.push([
+                        vertex_direction_vec.x,
+                        vertex_direction_vec.y,
+                        vertex_direction_vec.z,
+                    ]);
+
+                    if !(is_right_edge || is_bottom_edge) {
+                        index_values.extend([
+                            current_index,
+                            current_index + resolution as u32,
+                            current_index + 1,
+                            current_index + resolution as u32,
+                            current_index + resolution as u32 + 1,
+                            current_index + 1,
+                        ]);
+                    }
+
+                    current_index += 1;
+                }
+            }
+        }
+
+        mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
+        mesh.insert_attribute(
+            Mesh::ATTRIBUTE_NORMAL,
+            self.calculate_normals_for_mesh(&mesh, resolution as usize),
+        );
+        mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+        mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, colors);
+        mesh.set_indices(Some(Indices::U32(index_values)));
+        mesh.generate_tangents().unwrap();
+
+        mesh
+    }
+
+    fn calculate_normals_for_mesh(&self, mesh: &Mesh, resolution: usize) -> Vec<[f32; 3]> {
+        let mut normals: Vec<[f32; 3]> = vec![];
+        if let Some(VertexAttributeValues::Float32x3(vertices)) =
+            mesh.attribute(Mesh::ATTRIBUTE_POSITION)
+        {
+            for (i, vertex) in vertices.iter().enumerate() {
+                let is_right_edge = i % resolution == resolution - 1;
+                let is_bottom_edge = i >= (resolution * (resolution - 1));
+
+                let vertex_vec = Vec3::new(vertex[0], vertex[1], vertex[2]);
+
+                let x_dir: i32 = if is_right_edge { -1 } else { 1 };
+                let y_dir: i32 = if is_bottom_edge { -1 } else { 1 };
+
+                let x_vertex = vertices[(i as i32 + x_dir) as usize];
+                let y_vertex = vertices[(i as i32 + y_dir * resolution as i32) as usize];
+
+                let x_dir_vec = Vec3::new(x_vertex[0], x_vertex[1], x_vertex[2]) - vertex_vec;
+                let y_dir_vec = Vec3::new(y_vertex[0], y_vertex[1], y_vertex[2]) - vertex_vec;
+
+                let normal = x_dir_vec.cross(y_dir_vec).normalize();
+
+                let normal_multiplier = normal.dot(Vec3::from(*vertex)).signum();
+
+                normals.push((normal * normal_multiplier).into());
+            }
+        }
+
+        normals
+    }
 }
 
-struct PlanetShape {}
+impl From<PlanetShape> for Mesh {
+    fn from(shape: PlanetShape) -> Self {
+        shape.generate_mesh()
+    }
+}
